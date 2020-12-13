829c4d2d2be152d072bbbf27bdf2edd6d2ecae49
KatsutoshiOtogawa <gh1ab7im@gmail.com>
2020-12-13 20:40:41 +0900

    update

--- README.md
+++ README.md
@@ -7,6 +7,21 @@ this project has Vagrantfile.
 vagrant up
 ```
 sqlserver-fedora environment is being launch.
+
+# install sample Database AdventureWorks2019 and AdventureWorksDW2019
+
+```
+# change user and read .bash_profile.
+sudo su
+source ~/.bash_profile
+
+# execute bash function.
+enable_sampleschema
+
+# if you uninstall schema, execute below function
+disable_sampleschema
+```
+
 # if you want to create vagrant box from vagrant file.
 
 ```
--- provision.sh
+++ provision.sh
@@ -1,5 +1,15 @@
 dnf -y update
 
+# install firewalld. firewalld is rhel,centos default dynamic firewall.
+dnf -y install firewalld
+
+# enabla firewalld.
+systemctl enable firewalld
+systemctl start firewalld
+
+# port forwarding sqlserver port 1433.
+firewall-cmd --add-port=1433/tcp --zone=public --permanent
+
 # install expect and pexpect for silent install.
 dnf install -y expect
 pip3 install pexpect
@@ -59,6 +69,64 @@ echo '# set sqlserver environment variable'  >> ~/.bash_profile
 echo export PATH=\$PATH:/opt/mssql-tools/bin >> ~/.bash_profile
 echo export SQLSERVER_PASSWORD=$SQLSERVER_PASSWORD >> ~/.bash_profile
 echo '' >> ~/.bash_profile
+
+cat << EOF >> ~/.bash_profile
+# create sample from github
+# reference from [Microsoft site: AdventureWorks sample databases](https://docs.microsoft.com/en-us/sql/samples/adventureworks-install-configure?view=sql-server-ver15&tabs=tsql)
+# reference from [Microsoft site: Migrate from windows to linux](https://docs.microsoft.com/en-us/sql/linux/sql-server-linux-migrate-restore-database?view=sql-server-ver15)
+# you want to know this script detail, go to https://github.com/microsoft/sql-server-samples.git
+function enable_sampleschema () {
+    # sample respository is huge. get recent coomit only.
+    mkdir \$HOME/sql-server-samples
+    cd \$HOME/sql-server-samples
+    curl -o AdventureWorks2019.bak -L https://github.com/Microsoft/sql-server-samples/releases/download/adventureworks/AdventureWorks2019.bak
+    # move data directory
+    mv AdventureWorks2019.bak /var/opt/mssql/data/
+    sqlcmd -S localhost -U SA -P \$SQLSERVER_PASSWORD << EOF2
+    RESTORE DATABASE [AdventureWorks2019]
+    FROM DISK = '/var/opt/mssql/data/AdventureWorks2019.bak'
+    WITH MOVE 'AdventureWorks2017' TO '/var/opt/mssql/data/AdventureWorks2019.mdf',
+    MOVE 'AdventureWorks2017_log' TO '/var/opt/mssql/data/AdventureWorks2019_log.ldf',
+    FILE = 1,  NOUNLOAD,  STATS = 5
+    GO
+EOF2
+    curl -o AdventureWorksDW2019.bak -L https://github.com/Microsoft/sql-server-samples/releases/download/adventureworks/AdventureWorksDW2019.bak
+    # move data directory
+    mv AdventureWorksDW2019.bak /var/opt/mssql/data/
+    sqlcmd -S localhost -U SA -P \$SQLSERVER_PASSWORD << EOF2
+    USE [master]
+    RESTORE DATABASE [AdventureWorksDW2019]
+    FROM DISK = '/var/opt/mssql/data/AdventureWorksDW2019.bak'
+    WITH MOVE 'AdventureWorksDW2017' TO '/var/opt/mssql/data/AdventureWorksDW2019.mdf',
+    MOVE 'AdventureWorksDW2017_log' TO '/var/opt/mssql/data/AdventureWorksDW2019_log.ldf',
+    FILE = 1,  NOUNLOAD,  STATS = 5
+    GO
+EOF2
+    cd - >> /dev/null
+    rm -rf \$HOME/sql-server-samples
+
+}
+
+function disable_sampleschema () {
+    # drop sample database.
+    sqlcmd -S localhost -U SA -P \$SQLSERVER_PASSWORD << EOF2
+    USE [master];
+    IF DB_ID('AdventureWorks2019') IS NOT NULL
+    BEGIN
+        DROP DATABASE AdventureWorks2019
+    END
+    IF DB_ID('AdventureWorksDW2019') IS NOT NULL
+    BEGIN
+        DROP DATABASE AdventureWorksDW2019
+    END
+    GO
+    # remove sample backup file.
+    rm -f /var/opt/mssql/data/AdventureWorks2019.bak /var/opt/mssql/data/AdventureWorksDW2019.bak
+EOF2
+
+}
+
+EOF
 
 # erase fragtation funciton. this function you use vagrant package.
 cat << END >> ~/.bash_profile
