ee63b657a25551cb27c152359bc380d9bcec70cd
KatsutoshiOtogawa <gh1ab7im@gmail.com>
2020-12-14 04:26:49 +0900

    update

--- README.md
+++ README.md
@@ -3,23 +3,39 @@
 refference from [Microsoft Document how to install sqlserver in rhel](https://docs.microsoft.com/en-us/sql/linux/quickstart-install-connect-red-hat?view=sql-server-ver15)
 # example script
 this project has Vagrantfile.
-```
+```shell
 vagrant up
 ```
 sqlserver-fedora environment is being launch.
 
-# install sample Database AdventureWorks2019 and AdventureWorksDW2019
+# install sample Database 
 
-```
+## AdventureWorks2019 and AdventureWorksDW2019
+
+```shell
 # change user and read .bash_profile.
 sudo su
 source ~/.bash_profile
 
 # execute bash function.
-enable_sampleschema
+enable_AdventureWorks
 
-# if you uninstall schema, execute below function
-disable_sampleschema
+# if you drop database, execute below function
+disaable_AdventureWorks
+```
+
+## WideWorldImporters and WideWorldImportersDW
+
+```shell
+# change user and read .bash_profile.
+sudo su
+source ~/.bash_profile
+
+# execute bash function.
+enable_WideWorldImporters
+
+# if you drop database, execute below function
+disable_WideWorldImporters
 ```
 
 # if you want to create vagrant box from vagrant file.
--- provision.sh
+++ provision.sh
@@ -10,6 +10,9 @@ systemctl start firewalld
 # port forwarding sqlserver port 1433.
 firewall-cmd --add-port=1433/tcp --zone=public --permanent
 
+# reload firewall settings.
+firewall-cmd --reload
+
 # install expect and pexpect for silent install.
 dnf install -y expect
 pip3 install pexpect
@@ -71,14 +74,12 @@ echo export SQLSERVER_PASSWORD=$SQLSERVER_PASSWORD >> ~/.bash_profile
 echo '' >> ~/.bash_profile
 
 cat << EOF >> ~/.bash_profile
-# create sample from github
-# reference from [Microsoft site: AdventureWorks sample databases](https://docs.microsoft.com/en-us/sql/samples/adventureworks-install-configure?view=sql-server-ver15&tabs=tsql)
 # reference from [Microsoft site: Migrate from windows to linux](https://docs.microsoft.com/en-us/sql/linux/sql-server-linux-migrate-restore-database?view=sql-server-ver15)
 # you want to know this script detail, go to https://github.com/microsoft/sql-server-samples.git
-function enable_sampleschema () {
-    # sample respository is huge. get recent coomit only.
-    mkdir \$HOME/sql-server-samples
-    cd \$HOME/sql-server-samples
+function enable_AdventureWorks () {
+    # reference from [Microsoft site: AdventureWorks sample databases](https://docs.microsoft.com/en-us/sql/samples/adventureworks-install-configure?view=sql-server-ver15&tabs=tsql)
+    mkdir \$HOME/AdventureWorks
+    cd \$HOME/AdventureWorks
     curl -o AdventureWorks2019.bak -L https://github.com/Microsoft/sql-server-samples/releases/download/adventureworks/AdventureWorks2019.bak
     # move data directory
     mv AdventureWorks2019.bak /var/opt/mssql/data/
@@ -103,11 +104,11 @@ EOF2
     GO
 EOF2
     cd - >> /dev/null
-    rm -rf \$HOME/sql-server-samples
+    rm -rf \$HOME/AdventureWorks
 
 }
 
-function disable_sampleschema () {
+function disable_AdventureWorks () {
     # drop sample database.
     sqlcmd -S localhost -U SA -P \$SQLSERVER_PASSWORD << EOF2
     USE [master];
@@ -120,10 +121,71 @@ function disable_sampleschema () {
         DROP DATABASE AdventureWorksDW2019
     END
     GO
+EOF2
     # remove sample backup file.
     rm -f /var/opt/mssql/data/AdventureWorks2019.bak /var/opt/mssql/data/AdventureWorksDW2019.bak
+}
+
+EOF
+
+cat << EOF >> ~/.bash_profile
+# reference from [Microsoft site: Migrate from windows to linux](https://docs.microsoft.com/en-us/sql/linux/sql-server-linux-migrate-restore-database?view=sql-server-ver15)
+# you want to know this script detail, go to https://github.com/microsoft/sql-server-samples.git
+function enable_WideWorldImporters () {
+    # reference from [Microsoft site: WideWorldImporters sample databases](https://docs.microsoft.com/en-us/sql/samples/wide-world-importers-dw-install-configure?view=sql-server-ver15)
+    mkdir \$HOME/WideWorldImporters
+    cd \$HOME/WideWorldImporters
+    curl -o WideWorldImporters-Full.bak -L https://github.com/Microsoft/sql-server-samples/releases/download/wide-world-importers-v1.0/WideWorldImporters-Full.bak
+    # move data directory
+    mv WideWorldImporters-Full.bak /var/opt/mssql/data/
+    sqlcmd -S localhost -U SA -P \$SQLSERVER_PASSWORD << EOF2
+    USE [master]
+    RESTORE DATABASE [WideWorldImporters]
+    FROM DISK = '/var/opt/mssql/data/WideWorldImporters-Full.bak'
+    WITH MOVE 'WWI_Primary' TO '/var/opt/mssql/data/WideWorldImporters.mdf',
+    MOVE 'WWI_UserData' TO '/var/opt/mssql/data/WideWorldImporters_UserData.ndf',
+    MOVE 'WWI_Log' TO '/var/opt/mssql/data/WideWorldImporters.ldf',
+    MOVE 'WWI_InMemory_Data_1' TO '/var/opt/mssql/data/WideWorldImporters_InMemory_Data_1',
+    FILE = 1,  NOUNLOAD,  STATS = 5
+    GO
+
 EOF2
 
+    curl -o WideWorldImportersDW-Full.bak -L https://github.com/Microsoft/sql-server-samples/releases/download/wide-world-importers-v1.0/WideWorldImportersDW-Full.bak
+    # move data directory
+    mv WideWorldImportersDW-Full.bak /var/opt/mssql/data/
+    sqlcmd -S localhost -U SA -P \$SQLSERVER_PASSWORD << EOF2
+    USE [master]
+    RESTORE DATABASE [WideWorldImportersDW]
+    FROM DISK = '/var/opt/mssql/data/WideWorldImportersDW-Full.bak'
+    WITH MOVE 'WWI_Primary' TO '/var/opt/mssql/data/WideWorldImportersDW.mdf',
+    MOVE 'WWI_UserData' TO '/var/opt/mssql/data/WideWorldImportersDW_UserData.ndf',
+    MOVE 'WWI_Log' TO '/var/opt/mssql/data/WideWorldImportersDW.ldf',
+    MOVE 'WWIDW_InMemory_Data_1' TO '/var/opt/mssql/data/WideWorldImportersDW_InMemory_Data_1',
+    FILE = 1,  NOUNLOAD,  STATS = 5
+    GO
+EOF2
+    cd - >> /dev/null
+    rm -rf \$HOME/WideWorldImporters
+
+}
+
+function disable_WideWorldImporters () {
+    # drop sample database WideWorldImporters.
+    sqlcmd -S localhost -U SA -P \$SQLSERVER_PASSWORD << EOF2
+    USE [master];
+    IF DB_ID('WideWorldImporters') IS NOT NULL
+    BEGIN
+        DROP DATABASE WideWorldImporters
+    END
+    IF DB_ID('WideWorldImportersDW') IS NOT NULL
+    BEGIN
+        DROP DATABASE WideWorldImportersDW
+    END
+    GO
+EOF2
+    # remove sample backup file.
+    rm -f /var/opt/mssql/data/WideWorldImporters-Full.bak /var/opt/mssql/data/WideWorldImportersDW-Full.bak
 }
 
 EOF
